aa201c562073072dd69ede5e0c66612fa836f4ea
andrianseptiadi <iosdevmicropiranti@gmail.com>
2022-02-21 15:53:49 +0700

    Revert "Update Magisk Detector"
    This reverts commit 762579cfb72676b50f9ee1264a9ab3ac3c4722c7.

--- android/src/main/kotlin/com/superpenguin/detect_magisk1/DetectMagisk1Plugin.kt
+++ android/src/main/kotlin/com/superpenguin/detect_magisk1/DetectMagisk1Plugin.kt
@@ -46,25 +46,7 @@ fun check(): Boolean {
         "/magisk",
         "/sbin_orig",
         "/data/magisk",
-        "/data/adb/magisk",
-        "/data/adb/magisk.db",
-        "/data/adb/magisk32",
-        "/data/adb/magisk64",
-        "/data/adb/magiskboot",
-        "/data/adb/magiskinit",
-        "/bin/magisk",
-        "/bin/magiskinit",
-        "/bin/magiskpolicy",
-//        "/data/data/io.github.huskydg.magiskonnox",
-//        "/data/user_de/0/io.github.huskydg.magiskonnox",
-        "/data/data/com.fox2code.mmm/cache/magisk_alt_repo",
-        "/data/data/com.fox2code.mmm/cache/magisk_repo",
-        "/etc/init/magisk",
-        "/etc/init/magisk/magisk32",
-        "/etc/init/magisk/magisk64",
-        "/etc/init/magisk/magiskboot",
-        "/etc/init/magisk/magiskinit",
-        "/etc/init/magisk.rc",
+        "/sbin/.core"
     )
 
     for (p in paths) {
